2000d7018fe1c4bcb45817347a493e325482c399
Alex Batistta <batistta.alex@outlook.com>
2025-06-01 15:01:14 -0300

    icons on buttons background

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const btnRules = document.querySelector('.rules-btn');
 const btnClose = document.querySelector('.close-btn');
 const modalRules = document.querySelector('.modal');
 
+// Show - Hide Rules Modal
 btnRules.addEventListener('click', () => {
   modalRules.classList.toggle('show-modal');
   btnRules.blur();
@@ -10,3 +11,50 @@ btnRules.addEventListener('click', () => {
 btnClose.addEventListener('click', () => {
   modalRules.classList.toggle('show-modal');
 });
+
+const CHOICES = [
+  {
+    name: 'paper',
+    beats: 'rock',
+  },
+  {
+    name: 'scissors',
+    beats: 'paper',
+  },
+  {
+    name: 'rock',
+    beats: 'scissors',
+  },
+];
+
+const gameDiv = document.querySelector('.game-container');
+const resultsDiv = document.querySelector('.results-container');
+const resultDivs = document.querySelectorAll('.results-result');
+
+document.querySelectorAll('.choice-btn').forEach((btn) => {
+  btn.addEventListener('click', () => userChoose(btn));
+});
+
+// Game Logic
+const userChoose = (button) => {
+  const choiceName = button.dataset.choice;
+  const choice = CHOICES.find((choice) => choice.name === choiceName);
+  setChoose(choice);
+};
+
+const setChoose = (choice) => {
+  displayResults([choice, aiChoose()]);
+};
+
+const aiChoose = () => {
+  const rand = Math.floor(Math.random() * CHOICES.length);
+  return CHOICES[rand];
+};
+
+function displayResults(results) {
+  gameDiv.classList.add('hidden');
+  resultsDiv.classList.remove('hidden');
+  void resultsDiv.offsetWidth;
+  resultsDiv.classList.add('show');
+  console.log(results);
+}
